f6504dc0390e3ff0020386fb174c25c7eb1ccad7
Indra2108 <fizipratama21@gmail.com>
2021-11-15 13:27:16 +0700

    Import styling

--- src/login/index.js
+++ src/login/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
+// import style.js
+import styles from './style'
+
 export default class Login extends Component {
     render() {
         return(
--- src/register/index.js
+++ src/register/index.js
@@ -1,6 +1,9 @@
 import React, {Component} from "react";
 import {View, Text} from 'react-native';
 
+// import style.js
+import styles from './style'
+
 export default class Register extends Component {
     render() {
         return(
