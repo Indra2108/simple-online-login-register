2fa4ec6a0504c56ca215bd4ea99eb71ea071dce1
Indra2108 <fizipratama21@gmail.com>
2021-11-17 08:58:23 +0700

    Add Dashboard screen

---
+++ src/dashboard/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    constainer: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
